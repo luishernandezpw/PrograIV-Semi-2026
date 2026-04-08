32d30871cb2914e22ba7460b384da0ad36d35947
Luis Hernandez <Estudiante@DESKTOP-J6D5H3A>
2026-04-08 14:28:39 -0600

    Creacion del controlador y el modelo de Alumno

--- academica/resources/views/welcome.blade.php
+++ academica/resources/views/welcome.blade.php
@@ -4,13 +4,47 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>::. Sistema Academico ..::</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+       <!-- CSS -->
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/alertify.min.css"/>
+        <!-- Default theme -->
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/themes/default.min.css"/>
+        <!-- Semantic UI theme -->
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/themes/semantic.min.css"/>
+        <!-- Bootstrap theme -->
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.14.0/build/css/themes/bootstrap.min.css"/>
+        <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"/>
     </head>
     <body class="antialiased">
-       Bienvenidos a Progra IV - Laravel
+        <div id="app">
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">::.. SISTEMA ACADEMICO ..::</a>
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link" href="#" @click="abrirVentana('alumnos')">Alumnos</a>
+                            <a class="nav-link" href="#" @click="abrirVentana('materias')">Materias</a>
+                            <a class="nav-link" href="#" @click="abrirVentana('docentes')">Docentes</a>
+                            <a class="nav-link" href="#" @click="abrirVentana('matriculas')">Matriculas</a>
+                            <a class="nav-link" href="#" @click="abrirVentana('inscripciones')">Inscripciones</a>
+                            <a class="nav-link" href="#" @click="hacerBackup()">Backup</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+            <div id="appSistema" class="container-fluid" style="position: absolute; min-height: 80vh;">
+                <alumnos @buscar='buscar("busqueda_alumnos","obtenerAlumnos")' :forms="forms" ref="alumnos" v-show="forms.alumnos.mostrar"></alumnos>
+                <buscar_alumnos @modificar='modificar("alumnos","modificarAlumno", $event)' :forms="forms" ref="busqueda_alumnos" v-show="forms.busqueda_alumnos.mostrar"></buscar_alumnos>
+
+                <materias @buscar='buscar("busqueda_materias","obtenerMaterias")' :forms="forms" ref="materias" v-show="forms.materias.mostrar"></materias>
+                <buscar_materias @modificar='modificar("materias","modificarMateria", $event)' :forms="forms" ref="busqueda_materias" v-show="forms.busqueda_materias.mostrar"></buscar_materias>
+
+                <docentes @buscar='buscar("busqueda_docentes","obtenerDocentes")' :forms="forms" ref="docentes" v-show="forms.docentes.mostrar"></docentes>
+                <buscar_docentes @modificar='modificar("docentes","modificarDocente", $event)' :forms="forms" ref="busqueda_docentes" v-show="forms.busqueda_docentes.mostrar"></buscar_docentes>
+            </div>
+        </div>
     </body>
 </html>
